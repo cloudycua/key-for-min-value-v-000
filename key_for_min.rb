dfe3a1a571ca6166c2bf9358af437c42c47267ed
Christine Cua <cloudycua@gmail.com>
2018-02-27 17:40:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
    smallest_value = Float::INFINITY
    smallest_key = {}
    if hash = ""
-     nil
+     smallest_key = nil
    else
      hash.each do |key, value|
        if value < smallest_value
